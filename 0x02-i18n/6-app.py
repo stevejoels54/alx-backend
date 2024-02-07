bf404375dc5b4d8ec28720641e0006b386873dd1
joelsteven <joelofelectronics@gmail.com>
2024-02-07 03:22:38 +0300

    Add: Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -34,10 +34,28 @@ def get_user(user_id) -> Union[Dict, None]:
 
 
 @app.before_request
-def before_request() -> None:
+def before_request():
     """Set the logged-in user"""
     user_id = request.args.get('login_as')
     g.user = get_user(int(user_id)) if user_id else None
+
+
+@babel.localeselector
+def get_locale():
+    locale_param = request.args.get('locale')
+    if locale_param and locale_param in app.config['LANGUAGES']:
+        return locale_param
+
+    if g.user:
+        user_locale = g.user.get('locale')
+        if user_locale and user_locale in app.config['LANGUAGES']:
+            return user_locale
+
+    request_locale = request.accept_languages.best_match(app.config['LANGUAGES'])
+    if request_locale:
+        return request_locale
+
+    return app.config['BABEL_DEFAULT_LOCALE']
 
 
 @app.route('/')
