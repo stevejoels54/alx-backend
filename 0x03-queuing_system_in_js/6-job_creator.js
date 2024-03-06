a1aee605a3d9e8f2c25abe7aee6a59b7b0906773
joelsteven <joelofelectronics@gmail.com>
2024-03-06 18:37:56 +0300

    Add job creator and job processor files

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import kue from "kue";
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: "1234567890",
+    message: "Hello, Steve Jobs!"
+};
+
+const job = queue.create("push_notification_code", jobData).save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on("complete", () => {
+    console.log("Notification job completed");
+});
+
+job.on("failed", () => {
+    console.log("Notification job failed");
+});
