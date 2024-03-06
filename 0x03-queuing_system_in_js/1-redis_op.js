dd35eb359778a20533786d86f9a59db1b7ffe89c
joelsteven <joelofelectronics@gmail.com>
2024-03-06 15:47:39 +0300

    Add Redis operations to queuing system

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,28 @@
+import redis from 'redis';
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, (err, reply) => {
+        redis.print(`Reply: ${reply}`);
+    });
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (err, reply) => {
+        console.log(reply);
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
